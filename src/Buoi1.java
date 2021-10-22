eae4b2c1678a25ba0d49c6bd67419ed68af0e5d1
hung <hoangquochung@gmail.com>
2021-10-22 19:27:10 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,35 @@
+public class Buoi1 {
+    // function
+    public static void main(String[] args) {
+        // kiểu dữ liệu
+        // String : kiểu chuỗi => 2 ký tự trở lên => "xin chào"
+        // int : kiểu số nguyên => đại diện cho số => 3
+        // float : kiểu số thực (số có phần thập phân 1.5) => 1.5
+        // boolean : kiểu lý luận (1 : true ; 2 : false)
+
+        // khai báo biến :
+        // type variable = value
+
+        String name = "Hoàng Quốc Hưng";
+        // age = 21 (number)
+        // address = Quận 11 (String)
+        // isMale (boolean)
+        // height : 1.78f;
+
+        int age = 21;
+        String address = "Biên Hòa - Đồng Nai";
+        boolean isMale = true;
+        float height = 1.78f;
+        String myFavorite = "Bóng đá";
+
+        System.out.println("Tên : " +name);
+        System.out.println("tuổi : " +age);
+        System.out.println("Địa chỉ :" +address);
+        System.out.println("Chiều cao : " +height + " m");
+        System.out.println("Sở thích : " +myFavorite);
+
+
+
+
+    }
+}
